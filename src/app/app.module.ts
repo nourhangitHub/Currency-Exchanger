a040171e0100befd708cec33e9794848608901f8
nourhangitHub <nourhankabsha@gmail.com>
2023-12-09 17:26:19 +0200

    add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { SharedModule } from '../app/shared/shared.module';
 @NgModule({
   declarations: [
     AppComponent
@@ -11,7 +11,7 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatSlideToggleModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    API_Key: "3c7a199542728973725fc17e4ac86bdb",
+    apiUrl: "https://data.fixer.io/api/"
+};
